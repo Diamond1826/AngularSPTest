2c11ceebb64ec6cfd68d8ff3c3d55554aaead99e
Diamond1826 <diamond1826@hotmail.com>
2017-05-17 19:53:54 +0200

    Alpha2.8

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-service',
@@ -10,6 +11,14 @@ export class ServiceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$(window).scroll(function() {
+  		$(".slideanim").each(function(){
+    		var pos = $(this).offset().top;
+    		var winTop = $(window).scrollTop();
+    		if (pos < winTop + 600) {
+      			$(this).addClass("slide");
+    		}
+  		});
+	}); 
   }
-
 }
